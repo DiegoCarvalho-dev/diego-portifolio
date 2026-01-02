7c104dac85023b609f528292cfd52e5f57a23a01
Diego Carvalho <diegoricardo2527@gmail.com>
2026-01-02 14:30:26 -0300

    feat: adiciona a sacao de habilidades

--- diego-portifolio/src/App.tsx
+++ diego-portifolio/src/App.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./sections/HeroSection";
 import ProjectsSection from "./sections/ProjectsSection";
+import SkillsSection from "./sections/SkillsSection";
 
 function App() {
   return (
     <div className="bg-black text-white font-sans">
       <HeroSection />
       <ProjectsSection />
+      <SkillsSection />
     </div>
   );
 }
